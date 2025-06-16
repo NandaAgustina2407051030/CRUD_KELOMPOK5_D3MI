9a5ba436f08899c242b7614ae066b52667fb13dc
UjangArmstrong <adzimf3@gmail.com>
2025-06-16 15:58:41 +0700

    Update SearchSort.cpp

--- SearchSort.cpp
+++ SearchSort.cpp
@@ -13,3 +13,16 @@ Pasien *SearchSort::cariPasienById(vector<Pasien> &daftar, int id)
     }
     return nullptr;
 }
+void SearchSort::urutkanPasienBerdasarkanUsia(vector<Pasien> &daftar)
+{
+    sort(daftar.begin(), daftar.end(), [](const Pasien &a, const Pasien &b)
+         { return a.usia < b.usia; });
+    cout << "Daftar pasien berhasil diurutkan berdasarkan usia.\n";
+}
+
+void SearchSort::urutkanPasienBerdasarkanNama(vector<Pasien> &daftar)
+{
+    sort(daftar.begin(), daftar.end(), [](const Pasien &a, const Pasien &b)
+         { return a.nama < b.nama; });
+    cout << "Daftar pasien berhasil diurutkan berdasarkan nama.\n";
+}
