3a7827aa1a302d6583016a58257cb356057dbe0a
UjangArmstrong <adzimf3@gmail.com>
2025-06-17 21:00:40 +0700

    Update main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include "CRUD.h"
+#include "Queue.h"
+#include "Stack.h"
+#include "SearchSort.h"
+
+using namespace std;
+
+void tampilkanMenu()
+{
+    cout << "\n=== MENU SISTEM MANAJEMEN PASIEN ===\n";
+    cout << "1. Tambah Pasien Baru\n";
+    cout << "2. Tampilkan Semua Pasien\n";
+    cout << "3. Ubah Data Pasien\n";
+    cout << "4. Hapus Pasien\n";
+    cout << "5. Tambah ke Antrian\n";
+    cout << "6. Tampilkan Antrian\n";
+    cout << "7. Layani Pasien (dari antrian)\n";
+    cout << "8. Tampilkan Riwayat\n";
+    cout << "9. Cari Pasien (by ID)\n";
+    cout << "10. Urutkan Pasien (by Usia)\n";
+    cout << "11. Urutkan Pasien (by Nama)\n";
+    cout << "0. Keluar\n";
+    cout << "Pilih menu: ";
+}
