8743afc35fde390ffb97310494b340cc641a393a
Amrullah Azzaky <resuslampung@gmail.com>
2025-06-19 10:54:38 +0700

    dokumentasi: menambahkan baris komentar pada program Stack.h

--- Stack.h
+++ Stack.h
@@ -7,8 +7,8 @@
 class Stack
 {
 public:
-    static void tambahRiwayat(stack<Pasien> &riwayat, const Pasien &pasien);
-    static void tampilkanRiwayat(stack<Pasien> riwayat);
+    static void tambahRiwayat(stack<Pasien> &riwayat, const Pasien &pasien); // Parameter: riwayat adalah stack Pasien yang akan ditambah, pasien adalah objek Pasien yang ingin ditambahkan
+    static void tampilkanRiwayat(stack<Pasien> riwayat); // Parameter: riwayat adalah stack Pasien yang berisi riwayat pasien
 };
 
 #endif
