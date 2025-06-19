007f6a278a358f9716d684ff1287802160bb767e
Fauzan <adzimf3@gmail.com>
2025-06-19 10:56:02 +0700

    Update SearchSort.h

--- SearchSort.h
+++ SearchSort.h
@@ -1,15 +1,16 @@
-#ifndef SEARCHSORT_H
-#define SEARCHSORT_H
+#ifndef SEARCHSORT_H //mencegah file header ini agar tidak disertakan lebih dari sekali
+#define SEARCHSORT_H 
 
-#include <vector>
+#include <vector> //untuk menyimpan kayak array dinamis
 #include "Pasien.h"
 
 class SearchSort
 {
 public:
-    static Pasien *cariPasienById(vector<Pasien> &daftar, int id);
-    static void urutkanPasienBerdasarkanUsia(vector<Pasien> &daftar);
-    static void urutkanPasienBerdasarkanNama(vector<Pasien> &daftar);
+    static Pasien *cariPasienById(vector<Pasien> &daftar, int id); //fungsi buat mencari pasien berdasarkan id
+    static void urutkanPasienBerdasarkanUsia(vector<Pasien> &daftar); //fungsi buat ngurutin pasien berdasarkan usia bisa dari terkecil atau terbesar tergantung implementasi
+    static void urutkanPasienBerdasarkanNama(vector<Pasien> &daftar); //buat ururtin berdasarkan nama
+// pakai static biar tidak perlu buat objek tinggal panggil aja
 };
 
 #endif
