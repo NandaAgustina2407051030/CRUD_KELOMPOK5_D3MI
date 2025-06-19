669127a9db48e99f76ae2e8b8e4d2da6e87ed5a5
Asti <120251775+SherlFish@users.noreply.github.com>
2025-06-19 08:29:56 +0700

    Update Queue.h
    menambahkan comment

--- Queue.h
+++ Queue.h
@@ -3,14 +3,14 @@
 
 #include <queue>
 #include <stack>
-#include "Pasien.h"
+#include "Pasien.h" // Gunakan header ini karena program ini membutuhkan struktur data Pasien.
 
-class Queue
+class Queue //ini adalah deklarasi class Queue, berisi 3 fungsi yaitu:
 {
 public:
-    static void tambahPasien(queue<Pasien> &antrian, const Pasien &pasien);
-    static void tampilkanAntrian(queue<Pasien> antrian);
-    static bool layaniPasien(queue<Pasien> &antrian, stack<Pasien> &riwayat);
+    static void tambahPasien(queue<Pasien> &antrian, const Pasien &pasien); //Menambahkan pasien ke antrian
+    static void tampilkanAntrian(queue<Pasien> antrian); //Menampilkan semua pasien dalam antrian
+    static bool layaniPasien(queue<Pasien> &antrian, stack<Pasien> &riwayat); //Melayani pasien pertama di antrian dan memasukkannya ke riwayat (stack)
 };
 
 #endif
